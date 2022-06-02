87d9057c8a182a36b6768840058925021b245b6d
Radhi Hudijan <radhif2009@gmail.com>
2022-06-02 18:08:25 +0200

    prepared statement is implemented for Ex3

--- Week3/homework/mongodb/Exercise3.js
+++ Week3/homework/mongodb/Exercise3.js
@@ -1,12 +1,8 @@
 function getPopulation(Country, name, code, cb) {
   // assuming that connection to the database is established and stored as conn
   conn.query(
-    `SELECT Population FROM` +
-      conn.escape(Country) +
-      `WHERE Name =` +
-      conn.escape(name) +
-      `and code = ` +
-      conn.escape(code),
+    `SELECT Population FROM ? WHERE Name = ? and code = ? `,
+    [Country, name, code],
     function (err, result) {
       if (err) cb(err);
       if (result.length == 0) cb(new Error("Not found"));
